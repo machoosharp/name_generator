4ddb07075552068cf9f920ce165f7c83c097b03b
machoosharp <sharpart555@yahoo.com>
2025-03-07 16:03:08 -0500

    Add Name Generator V1
    Adds code for first Name Generator conversion to python

---
+++ name_generator.py
@@ -0,0 +1,78 @@
+'''
+Name Generators: By msharp
+
+This file will contain all varients of my original name generator, originally
+a bash script I created to practice bash back in 2017. The premise was to
+generate names only by choosing a random next character based on the last character.
+
+The original script was about 80 lines long, and in 2025 when I decided to transfer
+it to python, I realized it only took about 3 lines to do so, which made me wonder
+how I could make it even more compact. This file is the list of iterations on
+that single function. Each name_generator function will have a number to show
+which version it is, and the docstring will describe the changes made in that version.
+'''
+import random
+import string
+
+
+# Original next char dict.
+# Key is previous letter
+# Value is list of letters which could come after the previous letter in any context
+next_char = {
+    'a': 'abcdefghijklmnopqrstuvwxyz',
+    'b': 'aeiloru',
+    'c': 'aehikloruyz',
+    'd': 'aeijoruy',
+    'e': 'abcdefghijklmnpqrstvwxyz',
+    'f': 'aeiloru',
+    'g': 'aehiloruy',
+    'h': 'aeiouy',
+    'i': 'abcdefgjklmnopqrstvwz',
+    'j': 'aeiou',
+    'k': 'aeilnoruy',
+    'l': 'aeilou',
+    'm': 'aeiouy',
+    'n': 'aeiou',
+    'o': 'abcdefghijklmnopqrstuvwxyz',
+    'p': 'aehilmnorsuy',
+    'q': 'u',
+    'r': 'aeiouy',
+    's': 'acehiklmnopqrstuw',
+    't': 'aehioruy',
+    'u': 'abcdefghijklmnoprstvwxyz',
+    'v': 'aeiou',
+    'w': 'aehioru',
+    'x': 'aeir',
+    'y': 'aeiou',
+    'z': 'aeiou',
+}
+
+def name_generator1( min_length=3, weight=0.7 ):
+    '''
+    Simplification of an old name generator I made for programming practice.
+    Generates names by first choosing a length, and then picking a random character.
+    The next character is randomly picked from a list character that might go after
+    the previous character. Nothing more complicated is done.
+
+    This generates some very fantacy like names, albiet sometimes hard to say. The
+    next character list was made by finding what chars could come after the key char
+    in almost any context without being too jarring to say. This was tweaked a bit
+    after running it multiple times to remove characters that seemed to produce
+    names that were too hard to say or wacky. This means that some real world names
+    are impossible to make simply because they may have an instance of character
+    pairs which are too context dependent, and can't exist in any random location
+    inside a word or name. I have nothing at all against those names, my name is
+    one of them. It just means your name is unique thats all.
+
+    Current parameters are that this only generates names between 3 and 9 chars, just
+    for simplicities sake. It also favors lengths 5 and 7 because of the weight value.
+    '''
+    name = ''
+    for idx in range( -1, int((random.randint(1,9) * weight) + min_length) - 1 ):
+        name += random.choice( next_char.get( name[idx:], string.ascii_lowercase ) )
+    return name.title()
+
+
+if __name__ == '__main__':
+    print( 'Name Generator V1:' )
+    print( name_generator1() )
